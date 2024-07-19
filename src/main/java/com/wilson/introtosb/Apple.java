eee825bf9c4fa779c43de643d853a3620aeed7f4
Wilson Rangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-07-19 22:30:33 +0200

    new commit

---
+++ src/main/java/com/wilson/introtosb/Apple.java
@@ -0,0 +1,11 @@
+package com.wilson.introtosb;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    public void eatApple(){
+        System.out.println("I am eating an apple");
+    }
+}
--- src/main/java/com/wilson/introtosb/IntroToSbApplication.java
+++ src/main/java/com/wilson/introtosb/IntroToSbApplication.java
@@ -1,13 +1,25 @@
 package com.wilson.introtosb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroToSbApplication {
+public class IntroToSbApplication implements CommandLineRunner {
+
+    @Autowired
+    Apple obj;
 
     public static void main(String[] args) {
+
         SpringApplication.run(IntroToSbApplication.class, args);
+
+
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        obj.eatApple();
+    }
 }
