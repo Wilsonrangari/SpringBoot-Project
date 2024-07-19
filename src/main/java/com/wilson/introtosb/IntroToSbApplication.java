4e33cfc5b4bb49680042ae15f88c3384ddd28097
Wilson Rangari <98660886+Wilsonrangari@users.noreply.github.com>
2024-07-19 22:18:18 +0200

    project setup commit

---
+++ src/main/java/com/wilson/introtosb/IntroToSbApplication.java
@@ -0,0 +1,13 @@
+package com.wilson.introtosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroToSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wilson/introtosb/IntroToSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.introtosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
